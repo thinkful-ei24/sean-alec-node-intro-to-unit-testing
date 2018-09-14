9ce2312c33153a9bf16b496112346be15d162427
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-09-14 08:52:22 -0700

    fizz buzz done

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -13,4 +13,23 @@ describe('fizzBuzzer', () => {
       expect(result).to.equal('fizz');
     });
   });
+
+ it('should only return buzz when you git it a number divisible by just 5' , ()=>{
+   const numbers = [5, 10, 25];
+   numbers.forEach((number) =>{
+     const result = fizzBuzzer(number);
+     expect(result).to.equal('buzz');
+   });
+ });
+
+ it('should only return fizz-buzz when you git it a number divisible by 5 and 3' , ()=>{
+   const numbers = [15,  30];
+   numbers.forEach((number) =>{
+     const result = fizzBuzzer(number);
+     expect(result).to.equal('fizz-buzz');
+   });
+ });
+
+
+
 });
