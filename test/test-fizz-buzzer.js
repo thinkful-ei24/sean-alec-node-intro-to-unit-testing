8a6e7dc87253d1f3abd12925126b7356a1bde45f
Sean Phelan <seanphelan97@gmail.com>
2018-09-14 09:40:50 -0600

    add fizz test

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -6,5 +6,11 @@ const expect = chai.expect;
 
 describe('fizzBuzzer', () => {
 
-  it('')
+  it('should only return fizz when given a number divisible by just 3', () => {
+    const numbers = [3, 9, 231];
+    numbers.forEach((number) => {
+      const result = fizzBuzzer(number);
+      expect(result).to.equal('fizz');
+    });
+  });
 });
